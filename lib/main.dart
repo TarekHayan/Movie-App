1d362d9c8f05bd3a84916eec23ca6f4f2418658b
TarekHayan <tarekhayan77@gmail.com>
2025-10-16 16:07:33 +0300

    add: add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/firebase_options.dart';
 import 'UI/screens/welcom_screen.dart';
 import 'UI/widgets/buttom_navigator_bar.dart';
 import 'logic/cubit/cubit/add_fav_cubit.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MovieApp());
 }
 
